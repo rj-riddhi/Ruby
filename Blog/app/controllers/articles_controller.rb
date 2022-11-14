2208d4fb7b7d304be7a28a1a07d3d9413b12bc21
rj <69528706+rj-riddhi@users.noreply.github.com>
2022-11-14 19:05:16 +0530

    basic crud

--- Blog/app/controllers/articles_controller.rb
+++ Blog/app/controllers/articles_controller.rb
@@ -8,18 +8,40 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params_secure)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params_secure)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path,status: :see_other
+  end
+
+  private
+  def article_params_secure
+    params.require(:article).permit(:title, :body)
   end
 
 end
