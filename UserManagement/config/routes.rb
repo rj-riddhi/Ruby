af498c422494b5189a2f9cc9e9f3490aa7b76a40
rj <69528706+rj-riddhi@users.noreply.github.com>
2022-11-15 19:23:35 +0530

    commit

--- UserManagement/config/routes.rb
+++ UserManagement/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
-  resources :friends
-  get 'home/about'
+  # resources :friends
+  get 'home/index'
   root 'home#index'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
