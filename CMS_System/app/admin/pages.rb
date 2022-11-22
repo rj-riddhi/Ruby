15b9d3fe911b8e6af5e40b103e1e098ae6a47f30
rj <69528706+rj-riddhi@users.noreply.github.com>
2022-11-22 12:23:58 +0530

    cms done

--- CMS_System/app/admin/pages.rb
+++ CMS_System/app/admin/pages.rb
@@ -1,5 +1,14 @@
 ActiveAdmin.register Page do
 
   permit_params :title, :slug, :description
+ 
+    form title: 'Edit Page' do |f|
+      inputs 'Details' do
+        input :title
+        input :slug
+        input :description, as: :ckeditor
+      end
+        actions
+    end
   
 end
--- CMS_System/db/schema.rb
+++ CMS_System/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_091437) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_052156) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -35,6 +35,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_091437) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "data_fingerprint"
+    t.string "type", limit: 30
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
   create_table "pages", force: :cascade do |t|
